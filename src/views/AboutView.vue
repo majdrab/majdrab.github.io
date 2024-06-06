3678184071acf200b55d0ab8672bd47d7b53031a
Maj Drab <maj.drab@gmail.com>
2024-06-06 10:11:04 +0200

    feat: add raw text to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,7 +5,29 @@
 <template>
   <div class="about">
     <BaseContainer>
-      <h2 class="">This is About page</h2>
+      <h2 class="">More about me</h2>
+      <p>I'm Maj Drab, a 29-year-old Slovenian junior Front-end developer. Currently in the process of gaining more programming knowledge and doing all kinds of projects.</p>
+      <p>I like to resolve design problems, create smart user interface and imagine useful interaction, developing rich web experiences & web applications.</p>
+      <p>When not working or futzing around with code, I like to play D&D, video and board games, or go for a walk in the woods.</p>
+      <p>I'm actively looking for a job.</p>
+      <p>
+        <a 
+          class="bg-underline text-secondary"
+          target="_blank"
+          href=""
+        >
+        Check out my CV
+        </a>
+      </p>
+    </BaseContainer>
+    <BaseContainer>
+      <h2>My Programming Path</h2>
+      <p>My programming path has begun with the beginning of the year 2022. Being unsatisfied with my pervious career choices made me tackle a new, undiscovered area of web development. Taking a leap of faith, I quit the previous job and started learning.</p>
+      <p>The beginning of this journey was hard, but exciting. Taking multiple classes about html, css, js and git on the learning platform Udemy worked wonders. With dedication and eagerness of obtaining more programming experience I managed to get an internship at the beginning of the summer that same year, in the company called Hudlajf.</p>
+      <p>Working in the Hudlajf team was an awesome experience, and under the tutorship of a menthor my understanding of code has greatly improved. During my time there I got to work with sass and my first js framework, vue, and with it the knowledge of code reusability, components, recursive components and overall organizing my code and the folder structure in applications. In november 2022, after several months our cooperation has sadly ended, because of the company's financial situation.</p>
+      <p>Through the next three months I have searched for my first proper job as a junior web developer. During that time I have been doing all kinds of smaller projects and improving my vue and js skills. In the beginning of february 2023 an opportunity showed itself and I got the role of junior web developer in the company Humanfrog.</p>
+      <p>Getting my first developer job was both hard and exciting at the same time. When the job interview for a place in Humanfrog team took place I was promised to be able to work with vue, with which I was familiar, but almost all of the work turned out to be in laravel blades and php. While being a bit dissapointed, I was lukcy to get a great menthor and with her help I've learned to work with the given technologies. Working on big, real projects pushed my knowledge to new level and i learned to collaborate with designers and backend developers. 
+      But the longer I worked in Humanfrog, the more pressure and negativity </p>
     </BaseContainer>
   </div>
 </template>
