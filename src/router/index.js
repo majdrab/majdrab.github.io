0776dc20b2ada575a90bffcee86a0375f94c6fa1
Maj Drab <maj.drab@gmail.com>
2024-08-27 11:59:21 +0200

    chore: clean up router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
@@ -12,37 +11,12 @@ const router = createRouter({
       name: 'home',
       components: {
         default: HomeView,
-        // short for LeftSidebar: LeftSidebar
         AboutView,
-        // they match the `name` attribute on `<router-view>`
         ProjectsView,
       },
     },
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // },
-    // {
-    //   path: '/projects',
-    //   name: 'projects',
-    //   component: () => import('../views/ProjectsView.vue')
-    // },
-    // {
-    //   path: '/animations',
-    //   name: 'animations',
-    //   component: () => import('../views/AnimationsView.vue')
-    // }
   ],
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(to) {
     if (to.hash) {
       return {
         el: to.hash,
