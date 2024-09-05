808af2c32a94d3891d08b7a7d41ddd6e231338ff
Maj Drab <maj.drab@gmail.com>
2024-09-06 01:42:40 +0200

    feat: update footer

--- src/components/layout/VFooter.vue
+++ src/components/layout/VFooter.vue
@@ -12,24 +12,24 @@ defineProps({
     <BaseContainer>
       <div class="footer__grid">
         <div>
-          <h4 class="text-gray-200 pb-2">Logo at some point</h4>
+          <h4>Illustration in progress</h4>
         </div>
-        <div class="">
-          <h4 class="text-gray-200 pb-2">Contacts</h4>
+        <div>
+          <h4 class="pb-2">Contacts</h4>
           <a 
             v-for="(contact, index) in contactsData" 
             :key="index" 
             :href="contact.href"
             target=”_blank”
-            class="text-gray-200 flex"
+            class=" flex"
           >
             <span :class="[`icon-${contact.icon}`, 'pr-1']"></span>
             <span class="bg-underline">{{ contact.title }}</span>
           </a>
         </div>
         <div>
-          <p class="text-gray-200">Copyright © 2024 Maj Drab</p>
-          <p class="text-gray-200">All Rights Reserved</p>
+          <p class="">Copyright © 2024 Maj Drab</p>
+          <p class="">All Rights Reserved</p>
         </div>
       </div>
     </BaseContainer>
@@ -38,13 +38,13 @@ defineProps({
 
 <style scoped>
 .footer {
-  @apply bg-primary flex-none pt-6 pb-12;
+  @apply bg-primary flex-none pt-6 pb-12 text-gray-200;
 
   .footer__grid {
     @apply grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-8 pb-4;
     /* [&>*]:border-t */
     > * {
-      @apply border-t border-gray-200 border-solid;
+      @apply pt-4 border-t border-gray-200 border-solid;
     }
   }
 }
